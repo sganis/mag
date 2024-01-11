b9549483d1c3dec1302ce00873427f1bde5fb69e
San <sganis@gmail.com>
2024-01-11 09:08:57 +0300

    v1.0.21

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -283,7 +283,7 @@
     box-shadow: 20px 20px 10px grey;
   }
   h1 {
-    background-color: darkgreen;
+    background-color: darkblue;
     color: white;
     padding: 20px;
     margin: 0;
--- deploy.py
+++ deploy.py
@@ -1,27 +1,49 @@
 # deploy to vercel with git
 # using cli: vercel --prod
+import sys
 import os
 import json
 import subprocess
+import semver
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 
 def run(cmd):
+
     subprocess.run(cmd, shell=True, check=True)
 
-# with open('./client/package.json') as r:
-#     js = json.loads(r.read())
-# version = js['version']
+def update_version(version):
+    with open(f'{DIR}/client/.env.production') as r:
+        prod = r.readlines()
+    with open(f'{DIR}/client/.env.production', 'wt') as w:
+        for line in prod:
+            if 'VITE_PUBLIC_VERSION=' in line:
+                w.write(f'VITE_PUBLIC_VERSION="{version}"\n')
+            else:
+                w.write(line)
 
-# with open('./api/version.txt') as r:
-#     version = r.read().strip()
-    
-with open(f'{DIR}/client/.env.production') as r:
-    for line in r:
-        if 'VERSION' in line:
-            version = line.split('=')[1].strip().strip('"')
+    with open(f'{DIR}/client/.env.development') as r:
+        prod = r.readlines()
+    with open(f'{DIR}/client/.env.development', 'wt') as w:
+        for line in prod:
+            if 'VITE_PUBLIC_VERSION=' in line:
+                w.write(f'VITE_PUBLIC_VERSION="{version}"\n')
+            else:
+                w.write(line)
+                
+def get_version():
+    with open(f'{DIR}/client/.env.production') as r:
+        for line in r:
+            if 'VERSION' in line:
+                return line.split('=')[1].strip().strip('"')
+    return ''
 
+version = get_version()
 assert version
+
+version = semver.Version.parse(version)
+version = version.bump_patch()
+update_version(version)
 
 print(f'deploying v{version}...')
 run('call npm --prefix ./client run build')
